14f496615785d8da69f5a398df32ca481541b659
Dio <primo.nicolas@outlook.be>
2015-04-16 10:21:12 +0200

    Premier commit: initialisation

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php echo '<h1>'."Projet Petit-Git".'</h1>' ?>
+    </body>
+</html>
